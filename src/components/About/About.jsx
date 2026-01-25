44907fb58f699058e5fc7727d20d63dc3b574fb6
RSF ROBIUL <115572383+Robiul3100@users.noreply.github.com>
2026-01-25 13:03:21 +0600

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/profile2.png';
+import profileImage from '../../assets/profile2.webp';
 
 const About = () => {
   return (
@@ -18,16 +18,16 @@ const About = () => {
           </h1>
           {/* Name */}
           <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
-            Mojahid Hassan
+            RSF ROBIUL
           </h2>
           {/* Skills Heading with Typing Effect */}
           <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
             <span className="text-white">I am a </span>
             <ReactTypingEffect
               text={[
-                'Fullstack Developer',
+                'Graphics Designer',
                 'App Developer',
-                ' Graphics Designer',
+                'Web Developer',
                 'Coder',
               ]}
               speed={100}
@@ -41,11 +41,11 @@ const About = () => {
           </h3>
           {/* About Me Paragraph */}
           <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
-Hi, I’m Mojahid. I build websites, tools, and digital experiences that are simple, clean, and actually helpful. I’m always learning, improving, and working on new ideas.
+Hi, I’m Robiul. I build websites, tools, and digital experiences that are simple, clean, and actually helpful. I’m always learning, improving, and working on new ideas.
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
@@ -54,7 +54,7 @@ Hi, I’m Mojahid. I build websites, tools, and digital experiences that are sim
               boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
             }}
           >
-            DOWNLOAD CV
+            Contract me
           </a>
           
         </div>
@@ -71,7 +71,7 @@ Hi, I’m Mojahid. I build websites, tools, and digital experiences that are sim
           >
             <img
               src={profileImage}
-              alt="Tarun Kaushik"
+              alt="RSF ROBIUL"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
           </Tilt>
